650199046f76f3d5575816b506f30956a6af75a9
Tarek <tarek.nawara@gmail.com>
2017-09-24 14:04:24 -0400

    fixed iterator problem in stack array based implementation

--- src/main/java/edu/interview/datastructures/linear/impl/StackArrayBasedImpl.java
+++ src/main/java/edu/interview/datastructures/linear/impl/StackArrayBasedImpl.java
@@ -6,11 +6,28 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Optional;
 
+/**
+ * Array based implementation of the stack data structure.
+ * <p>
+ *     In case of reaching the stack size we resize the stack
+ *     by doubling its size.
+ *     <t>The Amortized running time of all the operations is O(1)</t>
+ * </p>
+ *
+ * @param <T> type of elements inside the stack.
+ */
 public class StackArrayBasedImpl<T> implements MyStack<T> {
     private int capacity = 100;
     private Object[] a = new Object[capacity];
     private int top = -1;
 
+    /**
+     * Stack Iterator.
+     * <p>
+     *     This iterator copies the entire stack to protect the
+     *     iteration process from modifications of the stack at iteration time.
+     * </p>
+     */
     private class StackArrayBasedImplIterator implements Iterator<T> {
         private Object[] cloned = new Object[a.length];
         private int index = 0;
@@ -18,6 +35,7 @@ public class StackArrayBasedImpl<T> implements MyStack<T> {
         StackArrayBasedImplIterator() {
             System.arraycopy(a, 0, cloned, 0, a.length - 1);
         }
+
         @Override
         public boolean hasNext() {
             return index < cloned.length;
@@ -72,7 +90,7 @@ public class StackArrayBasedImpl<T> implements MyStack<T> {
 
     @Override
     public Iterator<T> iterator() {
-        return null;
+        return new StackArrayBasedImplIterator();
     }
 
     private void resize() {
